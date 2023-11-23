8a580b42e1fff2f78d99246b67a68fd360372692
ark1111 <alireza.khaji11@gmail.com>
2023-11-23 13:23:58 +0330

    feat : search

--- src/Components/Tree/index.tsx
+++ src/Components/Tree/index.tsx
@@ -1,46 +1,80 @@
-import { Input, Tree } from 'antd';
-import type { DataNode } from 'antd/es/tree';
-import React, { useContext, useMemo, useRef, useState } from 'react';
-import AppContext from '../../appContext';
-import { NodeType } from '../../types';
-import Node from './node';
-import SearchResult from './searchResult';
+import { Input, Tree } from "antd";
+import type { DataNode } from "antd/es/tree";
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
+import AppContext from "../../appContext";
+import { NodeType } from "../../types";
+import Node from "./node";
+import SearchResult from "./searchResult";
 
 const { Search } = Input;
 
 interface Props {
-  handleContextMenuClick: (key: string,node:NodeType) => void;
-  setSelectedItem : (node:NodeType) => void
+  handleContextMenuClick: (key: string, node: NodeType) => void;
+  setSelectedItem: (node: NodeType) => void;
 }
 
-const TreeExtended: React.FC<Props> = ({ handleContextMenuClick,setSelectedItem }) => {
+const TreeExtended: React.FC<Props> = ({
+  handleContextMenuClick,
+  setSelectedItem,
+}) => {
   const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
   const [autoExpandParent, setAutoExpandParent] = useState(true);
   const searchedKeyword = useRef();
   const [searchResultVisible, setSearchResultVisible] = useState(true);
   const { treeData } = useContext(AppContext);
-  
+  const [searchResult, setSearchResult] = useState([]);
+
   const onExpand = (newExpandedKeys: any[]) => {
-    console.log("---------------onExpand--------------------------------------------");
+    console.log(
+      "---------------onExpand--------------------------------------------"
+    );
     setExpandedKeys(newExpandedKeys);
     setAutoExpandParent(false);
   };
 
-  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const searchHandler = (list: NodeType[], query: string) => {
+    let newList: NodeType[] = [];
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].title.includes(query)) {
+        newList.push(list[i]);
+      }  
+      if (list[i].children.length > 0) {
+        let otherResults = searchHandler(list[i].children, query);
+        newList = [...newList, ...otherResults];
+      }
+    }
+    return newList;
+  };
 
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let results = searchHandler(treeData, e.target.value);
+    console.log("results");
+    console.log(results);
+    setSearchResult(results);
   };
 
   const handlePressEnter = () => {
-    setSearchResultVisible(true)
-  }
+    setSearchResultVisible(true);
+  };
 
   const titleRenderer = (node: NodeType) => {
-    return <Node node={node} handleContextMenuClick={handleContextMenuClick} setSelectedItem={setSelectedItem}/>
-  }
+    return (
+      <Node
+        node={node}
+        handleContextMenuClick={handleContextMenuClick}
+        setSelectedItem={setSelectedItem}
+      />
+    );
+  };
 
   return (
-    <div className='tree-wrap'>
-      <Search style={{ marginBottom: 8 }} placeholder="جستجو" onChange={handleSearchInputChange} onPressEnter={handlePressEnter} />
+    <div className="tree-wrap">
+      <Search
+        style={{ marginBottom: 8 }}
+        placeholder="جستجو"
+        onChange={handleSearchInputChange}
+        onPressEnter={handlePressEnter}
+      />
       <Tree
         onExpand={onExpand}
         expandedKeys={expandedKeys}
@@ -48,7 +82,7 @@ const TreeExtended: React.FC<Props> = ({ handleContextMenuClick,setSelectedItem 
         treeData={treeData}
         titleRender={titleRenderer}
       />
-      {searchResultVisible && <SearchResult items={[]} />}
+      {searchResultVisible && <SearchResult items={searchResult} />}
     </div>
   );
 };
--- src/Components/Tree/searchResult.tsx
+++ src/Components/Tree/searchResult.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { NodeType } from '../../types';
+import "./searchResult.css";
+import { NodeType } from "../../types";
+import OrgchartIcon from "../SvgIcons/orgchart";
 interface Props {
-	items: (NodeType & { hierarchy: string[] })[]
+  items: NodeType[];
 }
 
 function SearchResult({ items }: Props) {
-	return <div className='search-result' style={{height: 200, overflow: 'auto'}}>
-		{items.map(item => (
-			<div>
-				{item.title}
-			</div>
-		))
-		}
-	</div>
+  return (
+    <div className="search-result" style={{ height: 200, overflow: "auto" }}>
+      {items.map((item) => (
+        <div className="search-result-item">
+          <div>{item.title}</div>
+          <div className="search-result-item-icon">
+            <OrgchartIcon />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default SearchResult;
